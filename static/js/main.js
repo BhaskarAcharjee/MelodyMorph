11a678e8b9cff8c5e6a760200577a1f5b7c881f4
BhaskarAcharjee <bhaskaracharjee2002@gmail.com>
2023-07-12 02:08:55 +0530

    Multiple Visualization Mode Added

--- static/js/main.js
+++ static/js/main.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const playButton = document.getElementById("playButton");
   const stopButton = document.getElementById("stopButton");
   const visualizationCanvas = document.getElementById("visualization");
+  const sampleRateElement = document.getElementById("sampleRate");
+  const durationElement = document.getElementById("duration");
   const audioListButtons = document.getElementsByClassName("audio-item");
 
   let audioContext;
@@ -12,6 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let visualizationWidth;
   let visualizationHeight;
   let gradient;
+  let currentMode = "frequency-bars"; // Default visualization mode
 
   // Initialize the audio context
   const initAudioContext = () => {
@@ -22,38 +25,41 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Load and play the selected audio file
   const loadAudioFile = (audioPath) => {
-    if (audioSource) {
-      audioSource.stop();
-      audioSource.disconnect();
-    }
+    fetch(audioPath)
+      .then((response) => response.arrayBuffer())
+      .then((arrayBuffer) => {
+        audioContext.decodeAudioData(arrayBuffer, (buffer) => {
+          // Stop the currently playing audio (if any)
+          stopAudio();
 
-    const request = new XMLHttpRequest();
-    request.open("GET", audioPath, true);
-    request.responseType = "arraybuffer";
+          // Create a new audio source and connect it to the analyzer node
+          audioSource = audioContext.createBufferSource();
+          audioSource.buffer = buffer;
+          audioSource.connect(analyzerNode);
+          analyzerNode.connect(audioContext.destination);
 
-    request.onload = () => {
-      audioContext.decodeAudioData(request.response, (buffer) => {
-        // Create a new audio source and connect it to the analyzer node
-        audioSource = audioContext.createBufferSource();
-        audioSource.buffer = buffer;
-        audioSource.connect(analyzerNode);
-        analyzerNode.connect(audioContext.destination);
+          // Start playing the audio
+          audioSource.start();
 
-        // Start playing the audio
-        audioSource.start();
+          // Update the visualization
+          visualizeAudio();
 
-        // Update the sample rate and duration values
-        const sr = audioContext.sampleRate;
-        const duration = buffer.duration.toFixed(2);
-        document.getElementById("sampleRate").textContent = sr;
-        document.getElementById("duration").textContent = duration;
-
-        visualizeAudio();
+          // Update audio information
+          updateAudioInfo();
+        });
+      })
+      .catch((error) => {
+        console.log("Error loading audio file:", error);
       });
-    };
-
-    request.send();
   };
+
+  // Event listener for audio list buttons
+  for (let i = 0; i < audioListButtons.length; i++) {
+    audioListButtons[i].addEventListener("click", function () {
+      const audioPath = this.getAttribute("data-audio-path");
+      loadAudioFile(audioPath);
+    });
+  }
 
   // Stop the currently playing audio
   const stopAudio = () => {
@@ -82,8 +88,74 @@ document.addEventListener("DOMContentLoaded", () => {
     gradient.addColorStop(0.5, "#00ff00");
     gradient.addColorStop(1, "#0000ff");
 
-    const drawFrame = () => {
-      requestAnimationFrame(drawFrame);
+    const drawWaveform = () => {
+      requestAnimationFrame(drawWaveform);
+      analyzerNode.getByteTimeDomainData(dataArray);
+
+      visualizationContext.fillStyle = "#000";
+      visualizationContext.fillRect(
+        0,
+        0,
+        visualizationWidth,
+        visualizationHeight
+      );
+      visualizationContext.lineWidth = 2;
+      visualizationContext.strokeStyle = "#fff";
+      visualizationContext.beginPath();
+
+      const sliceWidth = visualizationWidth / bufferLength;
+      let x = 0;
+
+      for (let i = 0; i < bufferLength; i++) {
+        const v = dataArray[i] / 128.0;
+        const y = (v * visualizationHeight) / 2;
+
+        if (i === 0) {
+          visualizationContext.moveTo(x, y);
+        } else {
+          visualizationContext.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+      }
+
+      visualizationContext.lineTo(visualizationWidth, visualizationHeight / 2);
+      visualizationContext.stroke();
+    };
+
+    const drawSpectrogram = () => {
+      requestAnimationFrame(drawSpectrogram);
+      analyzerNode.getByteFrequencyData(dataArray);
+
+      visualizationContext.clearRect(
+        0,
+        0,
+        visualizationWidth,
+        visualizationHeight
+      );
+
+      const barWidth = (visualizationWidth / bufferLength) * 2.5;
+      let x = 0;
+
+      for (let i = 0; i < bufferLength; i++) {
+        const barHeight = (dataArray[i] / 255) * visualizationHeight;
+
+        visualizationContext.fillStyle = `hsl(${
+          (i / bufferLength) * 360
+        }, 100%, 50%)`;
+        visualizationContext.fillRect(
+          x,
+          visualizationHeight - barHeight,
+          barWidth,
+          barHeight
+        );
+
+        x += barWidth + 1;
+      }
+    };
+
+    const drawFrequencyBars = () => {
+      requestAnimationFrame(drawFrequencyBars);
       analyzerNode.getByteFrequencyData(dataArray);
 
       visualizationContext.clearRect(
@@ -111,18 +183,49 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     };
 
+    const draw3DVisualization = () => {
+      // Implementation for 3D visualization goes here
+    };
+
+    const drawFrame = () => {
+      switch (currentMode) {
+        case "waveform":
+          drawWaveform();
+          break;
+        case "spectrogram":
+          drawSpectrogram();
+          break;
+        case "frequency-bars":
+          drawFrequencyBars();
+          break;
+        case "3d-visualization":
+          draw3DVisualization();
+          break;
+        default:
+          drawWaveform();
+          break;
+      }
+    };
+
     drawFrame();
   };
 
-  // Event listener for audio list buttons
-  for (let i = 0; i < audioListButtons.length; i++) {
-    audioListButtons[i].addEventListener("click", function () {
-      const audioPath = this.getAttribute("data-audio-path");
-      loadAudioFile(audioPath);
-    });
-  }
+  // Update the audio information
+  const updateAudioInfo = () => {
+    const sampleRate = audioSource.buffer.sampleRate;
+    const duration = audioSource.buffer.duration.toFixed(2);
 
-  // Event listeners for buttons
+    sampleRateElement.textContent = sampleRate;
+    durationElement.textContent = duration;
+  };
+
+  // Switch the visualization mode
+  const switchVisualizationMode = (mode) => {
+    currentMode = mode;
+    visualizeAudio();
+  };
+
+  // Event listeners for play and stop buttons
   playButton.addEventListener("click", () => {
     const file = audioFileInput.files[0];
     if (file) {
@@ -132,6 +235,23 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   stopButton.addEventListener("click", stopAudio);
+
+  // Event listeners to the mode buttons
+  const modeButtons = document.querySelectorAll(".mode-button");
+  modeButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const mode = button.getAttribute("data-mode");
+      switchVisualizationMode(mode);
+    });
+  });
+
+  // Get the visualization canvas size
+  visualizationWidth = visualizationCanvas.clientWidth;
+  visualizationHeight = visualizationCanvas.clientHeight;
+
+  // Set the visualization canvas size
+  visualizationCanvas.width = visualizationWidth;
+  visualizationCanvas.height = visualizationHeight;
 
   // Initialize the audio context
   initAudioContext();
