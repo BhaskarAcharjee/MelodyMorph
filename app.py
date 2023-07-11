4cac44facad0ee140b153963c502b502b9c50a29
BhaskarAcharjee <bhaskaracharjee2002@gmail.com>
2023-07-11 23:51:23 +0530

    Audio Information added

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request, jsonify
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import io
+import base64
 
 app = Flask(__name__)
 
@@ -9,9 +14,43 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload():
     audio_file = request.files['audio']
-    # Save the uploaded file to the uploads folder and process it
-    # Implement audio analysis and visualization generation logic here
+    # Save the uploaded file to the uploads folder
+    audio_file.save('uploads/audio.wav')
+
+    # Process the uploaded audio file
+    y, sr = librosa.load('uploads/audio.wav')
+
+    # Extract audio features
+    chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr)
+    spectral_centroid = librosa.feature.spectral_centroid(y=y, sr=sr)
+
+    # Create visualizations
+    plt.figure(figsize=(10, 6))
+
+    plt.subplot(3, 1, 1)
+    librosa.display.specshow(chroma_stft, y_axis='chroma')
+    plt.title('Chromagram')
+    plt.colorbar(format='%+2.0f dB')
+
+    plt.subplot(3, 1, 2)
+    librosa.display.specshow(mfcc, x_axis='time')
+    plt.title('MFCC')
+    plt.colorbar()
+
+    plt.subplot(3, 1, 3)
+    plt.plot(spectral_centroid[0])
+    plt.title('Spectral Centroid')
+
+    # Convert the visualization to base64-encoded image
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    base64_img = base64.b64encode(buf.getvalue()).decode('utf-8')
+    buf.close()
+
     # Return the visualization data as JSON response
+    return jsonify({'image': base64_img})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,88 +1,119 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const audioFileInput = document.getElementById('audioFile');
-    const playButton = document.getElementById('playButton');
-    const stopButton = document.getElementById('stopButton');
-    const visualizationCanvas = document.getElementById('visualization');
-  
-    let audioContext;
-    let audioSource;
-    let analyzerNode;
-    let visualizationContext;
-    let visualizationWidth;
-    let visualizationHeight;
-  
-    // Initialize the audio context
-    const initAudioContext = () => {
-      audioContext = new (window.AudioContext || window.webkitAudioContext)();
-      analyzerNode = audioContext.createAnalyser();
-      analyzerNode.fftSize = 256; // Adjust the FFT size as needed
+document.addEventListener("DOMContentLoaded", () => {
+  const audioFileInput = document.getElementById("audioFile");
+  const playButton = document.getElementById("playButton");
+  const stopButton = document.getElementById("stopButton");
+  const visualizationCanvas = document.getElementById("visualization");
+
+  let audioContext;
+  let audioSource;
+  let analyzerNode;
+  let visualizationContext;
+  let visualizationWidth;
+  let visualizationHeight;
+  let gradient;
+
+  // Initialize the audio context
+  const initAudioContext = () => {
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    analyzerNode = audioContext.createAnalyser();
+    analyzerNode.fftSize = 256; // Adjust the FFT size as needed
+  };
+
+  // Load and play the selected audio file
+  const loadAudioFile = () => {
+    const file = audioFileInput.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      audioContext.decodeAudioData(reader.result, (buffer) => {
+        // Stop the currently playing audio (if any)
+        stopAudio();
+
+        // Create a new audio source and connect it to the analyzer node
+        audioSource = audioContext.createBufferSource();
+        audioSource.buffer = buffer;
+        audioSource.connect(analyzerNode);
+        analyzerNode.connect(audioContext.destination);
+
+        // Start playing the audio
+        audioSource.start();
+
+        // Update the sample rate and duration values
+        const sr = audioContext.sampleRate;
+        const duration = buffer.duration.toFixed(2);
+        document.getElementById("sampleRate").textContent = sr;
+        document.getElementById("duration").textContent = duration;
+
+        visualizeAudio();
+      });
     };
-  
-    // Load and play the selected audio file
-    const loadAudioFile = () => {
-      const file = audioFileInput.files[0];
-      const reader = new FileReader();
-  
-      reader.onload = () => {
-        audioContext.decodeAudioData(reader.result, (buffer) => {
-          // Create a new audio source and connect it to the analyzer node
-          audioSource = audioContext.createBufferSource();
-          audioSource.buffer = buffer;
-          audioSource.connect(analyzerNode);
-          analyzerNode.connect(audioContext.destination);
-  
-          // Start playing the audio
-          audioSource.start();
-          visualizeAudio();
-        });
-      };
-  
-      reader.readAsArrayBuffer(file);
-    };
-  
-    // Stop the currently playing audio
-    const stopAudio = () => {
-      if (audioSource) {
-        audioSource.stop();
+
+    reader.readAsArrayBuffer(file);
+  };
+
+  // Stop the currently playing audio
+  const stopAudio = () => {
+    if (audioSource) {
+      audioSource.stop();
+      audioSource.disconnect();
+    }
+  };
+
+  // Visualize the audio data
+  const visualizeAudio = () => {
+    const bufferLength = analyzerNode.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    // Set up the visualization canvas
+    visualizationContext = visualizationCanvas.getContext("2d");
+    visualizationWidth = visualizationCanvas.width;
+    visualizationHeight = visualizationCanvas.height;
+    gradient = visualizationContext.createLinearGradient(
+      0,
+      0,
+      0,
+      visualizationHeight
+    );
+    gradient.addColorStop(0, "#ff0000");
+    gradient.addColorStop(0.5, "#00ff00");
+    gradient.addColorStop(1, "#0000ff");
+
+    const drawFrame = () => {
+      requestAnimationFrame(drawFrame);
+      analyzerNode.getByteFrequencyData(dataArray);
+
+      visualizationContext.clearRect(
+        0,
+        0,
+        visualizationWidth,
+        visualizationHeight
+      );
+
+      const barWidth = (visualizationWidth / bufferLength) * 2.5;
+      let x = 0;
+
+      for (let i = 0; i < bufferLength; i++) {
+        const barHeight = (dataArray[i] / 255) * visualizationHeight;
+
+        visualizationContext.fillStyle = gradient;
+        visualizationContext.fillRect(
+          x,
+          visualizationHeight - barHeight,
+          barWidth,
+          barHeight
+        );
+
+        x += barWidth + 1;
       }
     };
-  
-    // Visualize the audio data
-    const visualizeAudio = () => {
-      const bufferLength = analyzerNode.frequencyBinCount;
-      const dataArray = new Uint8Array(bufferLength);
-  
-      // Set up the visualization canvas
-      visualizationContext = visualizationCanvas.getContext('2d');
-      visualizationWidth = visualizationCanvas.width;
-      visualizationHeight = visualizationCanvas.height;
-  
-      const drawFrame = () => {
-        requestAnimationFrame(drawFrame);
-        analyzerNode.getByteFrequencyData(dataArray);
-  
-        visualizationContext.clearRect(0, 0, visualizationWidth, visualizationHeight);
-        visualizationContext.fillStyle = '#ff0000'; // Adjust the visualization color as needed
-  
-        const barWidth = visualizationWidth / bufferLength;
-        let x = 0;
-  
-        for (let i = 0; i < bufferLength; i++) {
-          const barHeight = (dataArray[i] / 255) * visualizationHeight;
-  
-          visualizationContext.fillRect(x, visualizationHeight - barHeight, barWidth, barHeight);
-  
-          x += barWidth + 1;
-        }
-      };
-  
-      drawFrame();
-    };
-  
-    // Event listeners for buttons
-    playButton.addEventListener('click', loadAudioFile);
-    stopButton.addEventListener('click', stopAudio);
-  
-    // Initialize the audio context
-    initAudioContext();
-  });  
+
+    drawFrame();
+  };
+
+  // Event listeners for buttons
+  playButton.addEventListener("click", loadAudioFile);
+  stopButton.addEventListener("click", stopAudio);
+
+  // Initialize the audio context
+  initAudioContext();
+});
